bac653939753221f219a726c6f24de56368c4360
katewalsh66 <katewalsh66@gmail.com>
2013-02-07 21:47:45 +0000

    update

---
+++ lab - 7Feb13/password1v1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> password1v1.php</title>
+</head>
+
+<body>
+
+
+
+<h2>POSTED Details</h2> <br/>
+
+USERNAME THAT WAS ENTERED IS: <?php echo $_POST['USERNAME']; ?> <br/>
+PASSWORD THAT WAS ENTERED IS: <?php echo $_POST['PASSWORD']; ?> <br/>
+
+<br/>
+
+
+
+</body>
+</html>
---
+++ lab - 7Feb13/password1v2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> password1v2.php</title>
+</head>
+
+<body>
+
+
+
+<h2>POSTED Details</h2> <br/>
+
+<?php 
+extract( $_POST );
+
+print("USERNAME THAT WAS ENTERED IS: $USERNAME <br/>");
+print("PASSWORD THAT WAS ENTERED IS: $PASSWORD");
+
+?> 
+
+
+<br/>
+
+
+
+</body>
+</html>
