371cec39b19ad325598b8d2ba35da9acdcb0dfcc
katewalsh66 <katewalsh66@gmail.com>
2013-02-24 21:02:42 +0000

    new

---
+++ labs- 14feb/PHPcookie1read.php
@@ -0,0 +1,15 @@
+<?php
+   echo $_COOKIE["user1"];   // Retrieve & display a cookies value
+?>
+
+<html>
+<head> <title> PHPcookie1read.php - How to retrieve a Cookie value </title> </head>
+<body>
+
+<h1> PHP </h1>
+<h3> Retrieval of value (from Cookie named user1) </h3>
+
+
+</body>
+</html>
+
---
+++ labs- 14feb/PHPcookie1write.php
@@ -0,0 +1,15 @@
+<?php 
+     setcookie("user1", "Tom Murphy", time()+3600); // 60sec X 60min = 3600 (= 1hr)
+?>
+
+<html>
+<head> <title> PHPcookie1write.php - How to set a Cookie </title> </head>
+<body>
+
+<h1> PHP </h1>
+<br />
+<h3> A Cookie named user1 has been set (with a value of Tom Murphy & exp time of 1hr) </h3>
+
+</body>
+</html>
+
